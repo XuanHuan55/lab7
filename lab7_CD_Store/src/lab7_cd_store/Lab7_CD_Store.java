fca07f3a7ef3f1767ed14f652a776eeadd45d1bb
XuanHuan55 <166469185+XuanHuan55@users.noreply.github.com>
2024-06-14 11:24:47 +0700

    v1

---
+++ lab7_CD_Store/src/lab7_cd_store/Lab7_CD_Store.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7_cd_store;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Lab7_CD_Store {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new CD_Store().setVisible(true);
+    }
+    
+}
